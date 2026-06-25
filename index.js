56daf83e09141dec2f403b60a846a43833190694
Lucas Queiroz <lqvieira7@gmail.com>
2026-06-25 20:28:18 -0300

    feat: importa routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import router from './routes/Users.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
