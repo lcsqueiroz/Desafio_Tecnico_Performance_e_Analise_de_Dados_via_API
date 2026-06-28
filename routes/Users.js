f80c4c326cd270c5dcff121cbcaa660922d47e4c
Lucas Queiroz <lqvieira7@gmail.com>
2026-06-28 15:51:52 -0300

    feat: superUsers router

--- routes/Users.js
+++ routes/Users.js
@@ -12,9 +12,23 @@ router.post('/users', (req, res) => {
   fs.readFile('usuarios_1000.json', 'utf-8', (err, snapshot) => {
     if (err) res.status(400).json({ message: 'Sem Informações' });
     const userData = JSON.parse(snapshot);
-    users.push(userData);
+    users.push(...userData);
     return res.status(200).json({ message: 'Usuários enviados com sucesso' });
   });
 });
 
+router.get('/superusers', (req, res) => {
+  const superUsers = users
+    .filter((users) => {
+      return users.score >= 900 && users.active;
+    })
+    .map((users) => ({
+      id: users.id,
+      name: users.name,
+      idade: users.age,
+      score: users.score,
+      país: users.country,
+    }));
+  return res.status(200).json(superUsers);
+});
 export default router;
